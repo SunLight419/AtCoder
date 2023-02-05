daca3f0646f5b9daf82da4a00142cbf706539580
mintia0924 <63721414+SunLight419@users.noreply.github.com>
2023-02-05 16:21:47 +0900

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using P = pair<int, int>;
+//#define DEBUG
+
+int main() {
+    int n;
+    cin >> n;
+    ll a, b;
+    rep(i, n) {
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
+    return 0;
+
+}
